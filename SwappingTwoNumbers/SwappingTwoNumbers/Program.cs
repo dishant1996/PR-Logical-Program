c90c3947a26898040f771aba239a9b3d9b98c2a8
Dishant Dilip Yashwante <113447476+dishant1996@users.noreply.github.com>
2022-12-09 00:12:07 +0530

    LogicalProblems

---
+++ SwappingTwoNumbers/SwappingTwoNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System; 
+namespace SwappingTwoNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;  //Creating variable (a) and assigning (10) value to it.
+            int b = 20; //Creating variable (b) and assigning (20) value to it.
+            int temp; // Creating (temp) variable to store temporary value.
+            Console.WriteLine("Before swapping value of a={0},b={1}", a, b);//printing before swapping values
+
+            temp = a; // Storing (a) value to temp. //temp=10
+            a = b; // Storing (b) value to (a). //a=20
+            b = temp; // storing temp value in (b) //b=10
+
+            Console.WriteLine("After swapping value of a={0},b={1}", a, b);//printing after swapping values
+            Console.ReadLine();
+        }
+ 
+   }
+}
